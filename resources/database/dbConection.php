38819c5941fac7d815d26c6e8c930aa2faf139d0
Santi Gismondi <129218589+santigismondi@users.noreply.github.com>
2026-05-14 18:59:55 -0300

    ejecucion de creacion de tabla usuarios al iniciar app (index.php y dbConection)

--- resources/database/dbConection.php
+++ resources/database/dbConection.php
@@ -8,8 +8,21 @@
 
     if (!$conexion) {
         die ("Error de conexion: " . mysqli_connect_error());
-    }else{
-        echo ("EXITO! conexion conectada");
-    }
+    } else {
+        $sql_crear_tabla = "CREATE TABLE IF NOT EXISTS usuarios (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nombre VARCHAR(100) NOT NULL,
+            username VARCHAR(50) NOT NULL UNIQUE,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            password VARCHAR(255) NOT NULL,
+            genero VARCHAR(50) NOT NULL,
+            rol VARCHAR(50) DEFAULT 'Usuario'
+        )";
 
+        if (mysqli_query($conexion, $sql_crear_tabla)) {
+            // echo ("La tabla 'usuarios' está lista.");
+        } else {
+            die ("Error al crear la tabla automáticamente: " . mysqli_error($conexion));
+        }
+    }
 ?>
